b339f284a8ef1e9576b622c0786e091cb450173e
yanshuyu <zhishuyan@gmail.com>
2020-02-02 11:33:34 +0800

    add custom control demo.

---
+++ 11_CustomControl/11_CustomControl/SYRangeSlider.swift
@@ -0,0 +1,9 @@
+//
+//  SYRangeSlider.swift
+//  11_CustomControl
+//
+//  Created by sy on 2020/1/31.
+//  Copyright © 2020 sy. All rights reserved.
+//
+
+import Foundation
---
+++ 11_CustomControl/11_CustomControl/TestSYSliderViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  11_CustomControl
+//
+//  Created by sy on 2020/1/31.
+//  Copyright © 2020 sy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
